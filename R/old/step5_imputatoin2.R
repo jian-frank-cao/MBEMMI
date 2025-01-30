3b7d3f7a0cc5e5e8952875b806cbe8b965dd11d6
Jian Cao <jccit@caltech.edu>
2025-01-30 15:11:55 +0000

    update

---
+++ R/old/step5_imputatoin2.R
@@ -0,0 +1,458 @@
+## Packages --------------------------------------------------------------------
+# sudo spctl --global-disable
+# install.packages(c("tidyverse", "furrr", "randtoolbox", "prodlim",
+#                    "matlab", "tmvmixnorm", "MCMCpack", "Amelia"))
+library(parallel)
+n.cores = detectCores()
+library(tidyverse)
+library(furrr)
+plan(multisession, workers = n.cores - 1)
+set.seed(1234)
+
+source("./R/utility/MBEMMI.R")
+source("./R/utility/BMMI.R")
+source("./R/utility/EMB.R")
+
+## Read data -------------------------------------------------------------------
+# data_qcew = readRDS("./data/qcew_2digit_fixed.Rds")
+list_data_qcew = readRDS("./data/qcew_rnd_suppression.Rds")
+
+## Functions -------------------------------------------------------------------
+# Functions to partition data
+partition_data = function(data_qcew){
+  
+  blocks = 2:5 %>% 
+    as.list %>% 
+    set_names(paste0("Digit-", 2:5)) %>% 
+    future_map(
+      ~{
+        digit = .
+        data = data_qcew$suppressed$qtrly %>% 
+          filter(nchar(industry_code) %in% c(digit, digit + 1)) %>% 
+          mutate(
+            group = substr(industry_code, 1, digit),
+            n_digit = nchar(industry_code)
+          )
+        data$n_missing = is.na(data) %>% rowSums(.)
+        groups = data$group[data$n_missing > 0 &
+                            data$n_digit == digit + 1] %>% unique
+        data = data %>% 
+          filter(group %in% groups) %>% 
+          dplyr::select(-c("n_missing", "n_digit")) %>% 
+          group_by(group) %>%
+          group_split(
+            .,
+            .keep = FALSE
+          ) %>% 
+          set_names(groups)
+        data %>% 
+          future_map(
+            ~{
+              block = .
+              ind_digit = nchar(block$industry_code) == digit
+              ind_digit_1 = nchar(block$industry_code) == digit + 1
+              code_higher = block$industry_code[ind_digit]
+              code_lower = block$industry_code[ind_digit_1]
+              list(
+                detail = block %>%
+                  filter(industry_code %in% code_lower) %>% 
+                  dplyr::select(-c("industry_code", "industry_title")) %>% 
+                  t %>% 
+                  data.frame %>% 
+                  `colnames<-`(code_lower),
+                auxiliary = data_qcew$estab$qtrly %>%
+                  filter(industry_code %in% code_lower) %>% 
+                  dplyr::select(-c("industry_code", "industry_title")) %>% 
+                  t %>% 
+                  data.frame %>% 
+                  `colnames<-`(code_lower),
+                q_total = block %>%
+                  filter(industry_code %in% code_higher) %>% 
+                  dplyr::select(-c("industry_code", "industry_title")) %>% 
+                  t %>% 
+                  data.frame %>% 
+                  `colnames<-`(code_higher),
+                a_total = data_qcew$suppressed$annual %>%
+                  filter(industry_code %in% code_lower) %>% 
+                  dplyr::select(-c("industry_code", "industry_title")) %>% 
+                  # mutate_all(~ .*4) %>% 
+                  t %>% 
+                  data.frame %>% 
+                  `colnames<-`(code_lower),
+                a_q_total = data_qcew$suppressed$annual %>%
+                  filter(industry_code %in% code_higher) %>% 
+                  dplyr::select(-c("industry_code", "industry_title")) %>% 
+                  # mutate_all(~ .*4) %>% 
+                  t %>% 
+                  data.frame %>% 
+                  `colnames<-`(code_higher)
+              )
+            }
+          )
+      }
+    )
+  
+  return(blocks)
+}
+
+# Parallel Sequential Imputation
+PSI <- function(data_qcew, method = "MBEMMI", n_imp = 10,
+                parallel_imp = TRUE, parallel_level = FALSE, ...){
+  # define map type
+  fun_map_imp <- ifelse(parallel_imp, future_map, map)
+  fun_map_level <- ifelse(parallel_level, future_map2, map2)
+  
+  # partition data
+  blocks <- partition_data(data_qcew)
+  
+  # generate sobol sequence
+  n_time <- nrow(blocks[[3]][[1]][[1]])
+  n_block <- sum(sapply(blocks, length))
+  sobol_seq <- prepare_sobol(n = n_time * n_block, dim = n_imp * 5)
+  list_sobol_imp <- lapply(1:n_imp, function(i){
+    start_col <- (i - 1) * 5 + 1
+    end_col <- min(i * 5, n_imp * 5)
+    sobol_seq[, start_col:end_col, drop = FALSE]
+  })
+  
+  # parallel imputations
+  result_imp <- list_sobol_imp %>% 
+    set_names(paste0("imputation-", 1:n_imp)) %>% 
+    fun_map_imp(
+      .options=furrr_options(seed = TRUE),
+      ~{
+        sobol_imp <- .
+        data_imputed <- data_qcew$suppressed$qtrly
+        
+        # sequential levels
+        for (level in 1:length(blocks)) {
+          list_data_block <- blocks[[level]]
+          n_block_level <- length(list_data_block)
+          if (n_block_level == 0) next
+          
+          # prepare sobol sequences for blocks
+          list_sobol_block <- lapply(1:n_block_level, function(i){
+            start_row <- (i - 1) * n_time + 1
+            end_row <- min(i * n_time, n_block_level * n_time)
+            sobol_imp[start_row:end_row, , drop = FALSE]
+          })
+          sobol_imp <- sobol_imp[-(1:n_block_level * n_time), ]
+          
+          # parallel blocks
+          result_level <- fun_map_level(
+            list_data_block,
+            list_sobol_block,
+            ~{
+              data <- .x
+              sobol_block <- .y
+              code_higher <- colnames(data$q_total)
+              code_lower <- colnames(data$detail)
+              
+              # fill missing q_total
+              if (any(is.na(data$q_total))) {  
+                data$q_total <- data_imputed %>%
+                  filter(industry_code %in% code_higher) %>%
+                  dplyr::select(-c("industry_code", "industry_title")) %>%
+                  t() %>%
+                  as.data.frame() %>%
+                  dplyr::rename_all(~code_higher)
+              }
+              
+              # impute missing values
+              if (length(code_lower) > 1) {
+                if (method == "MBEMMI") {
+                  res <- MBEMMI(data_raw = data, 
+                         n_imp = 1,
+                         sobol_seq = sobol_block)[[1]]
+                  # if didn't converge, use new sobol_seq
+                  while (res$converged == FALSE) {
+                    res <- MBEMMI(data_raw = data, 
+                                  n_imp = 1,
+                                  sobol_seq = NULL)[[1]]
+                  }
+                  res[["data_final"]][[1]]
+                }else if(method == "BMMI"){
+                  BMMI(data, n_imputation = 1)[[1]]
+                }else if(method == "EMB"){
+                  res <- EMB(data, n_imputation = 1)[[1]]
+                  if (length(res) == 1) {
+                    res
+                  }else{
+                    data$detail
+                  }
+                }
+              }else{
+                list()
+              }
+            }
+          )
+          
+          # Loop over each name in data_level
+          for (item in names(list_data_block)) {
+            
+            # Extract column names for 'detail' and 'q_total'
+            code_lower <- colnames(list_data_block[[item]]$detail)
+            code_higher <- colnames(list_data_block[[item]]$q_total)
+            
+            # Identify rows in data_imputed that match code_lower/code_higher
+            ind_lower <- which(data_imputed$industry_code %in% code_lower)
+            ind_higher <- which(data_imputed$industry_code %in% code_higher)
+            
+            # Depending on the condition, update these rows
+            if (length(code_lower) > 1) {
+              data_imputed[ind_lower, 3:ncol(data_imputed)] <-
+                t(result_level[[item]])
+            } else {
+              data_higher <- data_imputed[ind_higher, 3:ncol(data_imputed)]
+              data_imputed[ind_lower, 3:ncol(data_imputed)] <- data_higher
+            }
+          }
+        }
+        data_imputed
+      }
+    )
+  
+  return(result_imp)
+}
+
+
+## Run -------------------------------------------------------------------------
+for (i in c(8,10)) {
+  cat(paste0("Data set: ", i, ", Method: MBEMMI ........ Start...\n"))
+  set.seed(1234)
+  result_MBEMMI = PSI(list_data_qcew[[i]], method = "MBEMMI", n_sim = 10)
+  saveRDS(result_MBEMMI, paste0("./data/imputations_MBEMMI_", i, ".Rds"))
+  cat(paste0("Data set: ", i, ", Method: MBEMMI ........ Done.\n"))
+}
+
+for (i in 1:length(list_data_qcew)) {
+  cat(paste0("Data set: ", i, ", Method: BMMI ........ Start...\n"))
+  set.seed(1234)
+  result_BMMI = PSI(list_data_qcew[[i]], method = "BMMI", n_sim = 10)
+  saveRDS(result_BMMI, paste0("./data/imputations_BMMI_", i, ".Rds"))
+  cat(paste0("Data set: ", i, ", Method: BMMI ........ Done.\n"))
+}
+
+for (i in 1:length(list_data_qcew)) {
+  cat(paste0("Data set: ", i, ", Method: EMB ........ Start...\n"))
+  set.seed(1234)
+  result_EMB = PSI(list_data_qcew[[i]], method = "EMB", n_sim = 10)
+  saveRDS(result_EMB, paste0("./data/imputations_EMB_", i, ".Rds"))
+  cat(paste0("Data set: ", i, ", Method: EMB ........ Done.\n"))
+}
+
+
+## Run Origibal QCEW -----------------------------------------------------------
+data_qcew = readRDS("./data/qcew_2digit_fixed.Rds")
+
+# Parallel Sequential Imputation
+PSI <- function(data_qcew, method = "MBEMMI", n_imp = 10,
+                parallel_imp = TRUE, parallel_level = FALSE, ...){
+  # define map type
+  fun_map_imp <- ifelse(parallel_imp, future_map, map)
+  fun_map_level <- ifelse(parallel_level, future_map2, map2)
+  
+  # partition data
+  blocks <- partition_data(data_qcew)
+  
+  # generate sobol sequence
+  n_time <- nrow(blocks[[3]][[1]][[1]])
+  n_block <- sum(sapply(blocks, length))
+  sobol_seq <- prepare_sobol(n = n_time * n_block, dim = n_imp * 5)
+  list_sobol_imp <- lapply(1:n_imp, function(i){
+    start_col <- (i - 1) * 5 + 1
+    end_col <- min(i * 5, n_imp * 5)
+    sobol_seq[, start_col:end_col, drop = FALSE]
+  })
+  
+  # parallel imputations
+  result_imp <- list_sobol_imp %>% 
+    set_names(paste0("imputation-", 1:n_imp)) %>% 
+    fun_map_imp(
+      .options=furrr_options(seed = TRUE),
+      ~{
+        sobol_imp <- .
+        data_imputed <- data_qcew$suppressed$qtrly
+        
+        # sequential levels
+        for (level in 1:length(blocks)) {
+          list_data_block <- blocks[[level]]
+          n_block_level <- length(list_data_block)
+          if (n_block_level == 0) next
+          
+          # prepare sobol sequences for blocks
+          list_sobol_block <- lapply(1:n_block_level, function(i){
+            start_row <- (i - 1) * n_time + 1
+            end_row <- min(i * n_time, n_block_level * n_time)
+            sobol_imp[start_row:end_row, , drop = FALSE]
+          })
+          sobol_imp <- sobol_imp[-(1:n_block_level * n_time), ]
+          
+          # parallel blocks
+          result_level <- fun_map_level(
+            list_data_block,
+            list_sobol_block,
+            ~{
+              data <- .x
+              sobol_block <- .y
+              code_higher <- colnames(data$q_total)
+              code_lower <- colnames(data$detail)
+              
+              # fill missing q_total
+              if (any(is.na(data$q_total))) {  
+                data$q_total <- data_imputed %>%
+                  filter(industry_code %in% code_higher) %>%
+                  dplyr::select(-c("industry_code", "industry_title")) %>%
+                  t() %>%
+                  as.data.frame() %>%
+                  dplyr::rename_all(~code_higher)
+              }
+              
+              # impute missing values
+              if (length(code_lower) > 1 & code_higher == "336") { ####
+                if (method == "MBEMMI") {
+                  res <- MBEMMI(data_raw = data, 
+                                n_imp = 1,
+                                sobol_seq = sobol_block)[[1]]
+                  # if didn't converge, use new sobol_seq
+                  while (res$converged == FALSE) {
+                    res <- MBEMMI(data_raw = data, 
+                                  n_imp = 1,
+                                  sobol_seq = NULL)[[1]]
+                  }
+                  res[["data_final"]][[1]]
+                }else if(method == "BMMI"){
+                  BMMI(data, n_imputation = 1)[[1]]
+                }else if(method == "EMB"){
+                  res <- EMB(data, n_imputation = 1, polytime = 3)[[1]]
+                  if (length(res) == 1) {
+                    res
+                  }else{
+                    data$detail
+                  }
+                }
+              }else{
+                list()
+              }
+            }
+          )
+          
+          # Loop over each name in data_level
+          for (item in names(list_data_block)) {
+            
+            # Extract column names for 'detail' and 'q_total'
+            code_lower <- colnames(list_data_block[[item]]$detail)
+            code_higher <- colnames(list_data_block[[item]]$q_total)
+            
+            # Identify rows in data_imputed that match code_lower/code_higher
+            ind_lower <- which(data_imputed$industry_code %in% code_lower)
+            ind_higher <- which(data_imputed$industry_code %in% code_higher)
+            
+            # Depending on the condition, update these rows
+            if (length(code_lower) > 1 & code_higher == "336") { ###
+              data_imputed[ind_lower, 3:ncol(data_imputed)] <-
+                t(result_level[[item]])
+            } else {
+              data_higher <- data_imputed[ind_higher, 3:ncol(data_imputed)]
+              data_imputed[ind_lower, 3:ncol(data_imputed)] <- data_higher
+            }
+          }
+        }
+        data_imputed
+      }
+    )
+  
+  return(result_imp)
+}
+
+cat(paste0("Data set: ", 0, ", Method: MBEMMI ........ Start...\n"))
+set.seed(1234)
+result_MBEMMI = PSI(data_qcew, method = "MBEMMI", n_sim = 10)
+saveRDS(result_MBEMMI, paste0("./data/imputations_MBEMMI_", 0, ".Rds"))
+cat(paste0("Data set: ", 0, ", Method: MBEMMI ........ Done.\n"))
+
+cat(paste0("Data set: ", 0, ", Method: BMMI ........ Start...\n"))
+set.seed(1234)
+result_BMMI = PSI(data_qcew, method = "BMMI", n_sim = 10)
+saveRDS(result_BMMI, paste0("./data/imputations_BMMI_", 0, ".Rds"))
+cat(paste0("Data set: ", 0, ", Method: BMMI ........ Done.\n"))
+
+blocks <- partition_data(data_qcew)
+data = blocks$`Digit-3`$`336`
+res_emb <- EMB(data, n_imputation = 10, polytime = 3)
+
+# cat(paste0("Data set: ", 0, ", Method: EMB ........ Start...\n"))
+# set.seed(12)
+# result_EMB = PSI(data_qcew, method = "EMB", n_sim = 10)
+# saveRDS(result_EMB, paste0("./data/imputations_EMB_", 0, ".Rds"))
+# cat(paste0("Data set: ", 0, ", Method: EMB ........ Done.\n"))
+
+mean_3361_MBEMMI = result_MBEMMI %>% 
+  future_map(
+    ~{
+      res = .
+      res %>% filter(industry_code == "3361") %>% 
+        select(-industry_code, -industry_title) %>% 
+        data.frame()
+    }
+  ) %>% 
+  do.call("rbind", .) %>% 
+  colMeans() %>% 
+  round(., 0) %>% 
+  data.frame()
+
+mean_3361_BMMI = result_BMMI %>% 
+  future_map(
+    ~{
+      res = .
+      res %>% filter(industry_code == "3361") %>% 
+        select(-industry_code, -industry_title) %>% 
+        data.frame()
+    }
+  ) %>% 
+  do.call("rbind", .) %>% 
+  colMeans() %>% 
+  round(., 0) %>% 
+  data.frame()
+
+mean_3361_EMB = res_emb %>% 
+  future_map(
+    ~{
+      res = .
+      if (length(res) > 1) {
+        res %>% select(`3361`)
+      }else{
+        data.frame(var = rep(NA_real_,20))
+      }
+    }
+  ) %>% 
+  do.call("cbind", .) %>% 
+  rowMeans(na.rm = TRUE) %>% 
+  round(., 0) %>% 
+  data.frame()
+
+compare_3361 = data.frame(
+  true = data_qcew$true$qtrly %>% filter(industry_code == "3361") %>% 
+    select(-industry_code, -industry_title)  %>% 
+    as.numeric(),
+  MBEMMI = mean_3361_MBEMMI$.,
+  BMMI = mean_3361_BMMI$.,
+  EMB = mean_3361_EMB$.
+)
+
+compare_3361$time = 1:20
+
+compare_3361 %>% 
+  ggplot() +
+  geom_line(aes(x = time, y = true), color = "red") +
+  geom_line(aes(x = time, y = MBEMMI), color = "green") +
+  geom_line(aes(x = time, y = BMMI), color = "blue") +
+  geom_line(aes(x = time, y = EMB), color = "orange")
+
+compare_3361 %>%
+  pivot_longer(cols = -time, names_to = "Method", values_to = "Value") %>%
+  ggplot(aes(x = time, y = Value, color = Method)) +
+  geom_line() +
+  scale_color_manual(values = c("true" = "red", "MBEMMI" = "green", "BMMI" = "blue", "EMB" = "orange")) +
+  labs(color = "Method") +
+  theme_minimal()
